aaa0441aef3a4d3c08189d46a907e1433da7c722
Gopikrishna Thirunavukarasu <gopithiru2604@gmail.com>
2025-07-22 01:41:04 +0530

    Delete utils directory

--- utils/paths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parents[1]
-DATA_DIR = ROOT / "data"
-DB_PATH = ROOT / "database" / "ecommerce.db"
-CONFIG_DEFAULT = ROOT / "config_example.yaml"
