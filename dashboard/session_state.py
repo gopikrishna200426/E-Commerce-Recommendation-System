c7f021ca0131286e573ce19cc73c43db65c46913
Gopikrishna Thirunavukarasu <gopithiru2604@gmail.com>
2025-07-22 01:13:37 +0530

    Create session_state.py

---
+++ dashboard/session_state.py
@@ -0,0 +1,12 @@
+# dashboard/session_state.py
+import streamlit as st
+
+
+def get_state(key, default=None):
+    if key not in st.session_state:
+        st.session_state[key] = default
+    return st.session_state[key]
+
+
+def set_state(key, value):
+    st.session_state[key] = value
